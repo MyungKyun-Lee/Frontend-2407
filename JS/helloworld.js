a2733e54cd4dd31d68ee36009840b197518cef85
MyungKyun Lee <goodb@naver.com>
2024-07-12 14:45:31 +0900

    addfile

---
+++ JS/helloworld.js
@@ -0,0 +1,23 @@
+console.log('hello World!!!');
+
+let sum=0;
+for (let i=1; i<=100; i++){
+    sum += i;
+}
+console.log('1에서 100까지의 합은 ',sum);
+
+
+
+
+var name = "홍길동";
+if (name !== "임꺾정") {
+    var name = "임꺾정";
+}
+console.log(name);
+
+
+let name2 = "홍길동";
+if (name2 !== "임꺾정") {
+    let name = "임꺾정";
+}
+console.log(name2);
